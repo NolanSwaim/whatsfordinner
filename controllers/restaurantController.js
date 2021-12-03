719d434fe7b64342bf80c8b3b0e5e65a50207ee7
nolanswaim <nolanswaim13@gmail.com>
2021-12-03 12:54:53 -0800

    final commit

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,19 +1,88 @@
-module.exports.viewAll = function(req, res, next) {
-    const restaurant =[ {
-        id: 1,
-        name: 'Pizza Hut',
-        image: 'https://i3.ypcdn.com/blob/e28d7b49f0249334684a0e98ad6b6d42eb211768_400x260_crop.jpg',
-        rating: 2,
-        category: 'Italian',
-        description:'jbjesbf jkcbse hj jbj esbfjk cbsehj jbj esbfjk cbsehj jbje sbfjk cbsehj jbjes bfjk cbsehj'
-    },
-    {   id: 2,
-        name: 'Not Pizza Hut',
-        image: 'https://i3.ypcdn.com/blob/e28d7b49f0249334684a0e98ad6b6d42eb211768_400x260_crop.jpg',
-        rating: 4,
-        category: 'Italian',
-        description: 'jbjesbf jkcbse hj jbj esbfjk cbsehj jbj esbfjk cbsehj jbje sbfjk cbsehj jbjes bfjk cbsehj'
+const {Restaurant} = require('../models')
+const categories = ['Italian','Fast Food'];
 
-    }];
-    res.render('index', {restaurants});
+module.exports.viewAll = async function (req, res) {
+    let searchCategories =['All'];
+    for(let i= 0; i<categories.length; i++){
+        searchCategories.push(categories[i]);
+    }
+    let restaurants;
+    let searchCategory = req.query.category || 'All';
+    let searchRandom = req.query.random || false;
+    if (searchCategory==='All'){
+        restaurants = await Restaurant.findAll();
+    } else {
+        restaurants = await Restaurant.findAll({
+            where: {
+                        category:searchCategory
+                    }
+        });
+    }
+    if (restaurants.length > 0 && searchRandom)  {
+        let randomIndex = getRandomInt(restaurants.length);
+        restaurants =[restaurants[randomIndex]];
+    }
+    res.render('index', {restaurants, categories:searchCategories, searchCategory, searchRandom});
+}
+
+
+module.exports.renderEditForm = async function(req, res){
+    console.log(req.params);
+    const restaurant = await Restaurant.findByPk(
+        req.params.id
+    );
+    res.render('edit' , {restaurant, categories});
+}
+module.exports.updateRestaurant = async function(req, res) {
+    await Restaurant.update(
+        {
+            name: req.body.name,
+            category: req.body.category,
+            rating: req.body.rating,
+            image: req.body.image,
+            description: req.body.description
+        },
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/');
+}
+
+module.exports.deleteRestaurant = async function(req,res) {
+    await Restaurant.destroy(
+        {
+            where:
+                {
+                    id:req.params.id
+                }
+        });
+    res.redirect('/');
+}
+
+module.exports.renderAddForm = function(req, res){
+    const restaurant = {
+        name: "",
+        description:"",
+        rating: 1,
+        image: "",
+        category: categories[0],
+    };
+    res.render('add', {restaurant, categories});
+}
+module.exports.addRestaurant = async function(req,res) {
+    await Restaurant.create(
+        {
+            name:req.body.name,
+            category: req.body.category,
+            rating: req.body.rating,
+            image: req.body.image,
+            description: req.body.description
+    });
+    res.redirect('/');
+}
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@ var router = express.Router();
 const restaurantController = require('../controllers/restaurantController');
 /* GET home page. */
 router.get('/', restaurantController.viewAll);
-
+router.get('/edit/:id', restaurantController.renderEditForm);
+router.get('/edit/:id', restaurantController.updateRestaurant);
+router.get('/delete/:id', restaurantController.deleteRestaurant);
+router.get('/add', restaurantController.renderAddForm);
+router.get('/add', restaurantController.addRestaurant);
 module.exports = router;
+
